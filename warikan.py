a3e98e8f2da0a4a07afa89ff841bacf358108614
yoshihiko-kawai <kawai@alumni.tus.ac.jp>
2019-09-19 14:41:42 +0900

    test

--- warikan.py
+++ warikan.py
@@ -1,13 +1,13 @@
 def main():
-
     amount = int(input("金額は？ >"))
     number_of_people = int(input("人数は？ >"))
 
-    payment = amount // number_of_people
-    remainder = amount % number_of_people
+    payment, remainder = divmod(amount, number_of_people)
 
     print("金額:" + str(amount) + "人数:" + str(number_of_people) + "一人当たり:" + str(payment) + "円" "端数:" + str(remainder))
-    #出力　f-stringという機能（f記法）python3.6~
+    # 出力　f-stringという機能（f記法）python3.6~
     print(f"一人当たり{payment}円です。端数は{remainder}円です。")
+
+
 if __name__ == '__main__':
     main()
